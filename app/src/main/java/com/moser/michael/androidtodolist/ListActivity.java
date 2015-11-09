b32178f8e0e380ce16bac62c0d96d847e20554ba
Michael Moser <michael.opper.moser@gmail.com>
2015-11-09 15:30:12 -0500

    finished tutorial 1

--- app/src/main/java/com/moser/michael/androidtodolist/ListActivity.java
+++ app/src/main/java/com/moser/michael/androidtodolist/ListActivity.java
@@ -1,29 +1,41 @@
 package com.moser.michael.androidtodolist;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ListActivity extends AppCompatActivity {
+
+    private Button btnCreate;
+    private Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                Intent createActivity = new Intent(getApplicationContext(), CreateActivity.class);
+                startActivity(createActivity);
+
+            }
+        });
+
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                Intent updateActivity = new Intent(getApplicationContext(), UpdateActivity.class);
+                startActivity(updateActivity);
+
             }
         });
     }
-
 }
