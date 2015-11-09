a3a38d2ecf265942dba17ef6cf320db8d69d2c59
Michael Moser <michael.opper.moser@gmail.com>
2015-11-09 16:06:13 -0500

    added Task

---
+++ app/src/main/java/com/moser/michael/androidtodolist/model/Task.java
@@ -0,0 +1,32 @@
+package com.moser.michael.androidtodolist.model;
+
+/**
+ * Created by michaelmoser on 11/9/15.
+ */
+public class Task {
+
+    public String taskId;
+    public String name;
+
+    public Task(){
+        this.taskId = "";
+        this.name = "";
+    }
+
+    public void setTaskId(String taskId){
+        this.taskId = taskId;
+    }
+
+    public String getTaskId(){
+        return taskId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
